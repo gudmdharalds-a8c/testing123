1c4dc8318ac6cb98c1eb3e5cacecd2752a76c6c3
Gudmundur Haraldsson <gdh1@hi.is>
2019-11-14 16:23:47 +0000

    test

--- bla-12.php
+++ bla-12.php
@@ -9,4 +9,5 @@ echo esc_html( 'foo: ' . time(). time() . PHP_EOL );
 echo esc_html( 'foo: ' . time(). time() . PHP_EOL );
 echo esc_html( 'foo: ' . time(). time() . PHP_EOL );
 echo esc_html( 'foo: ' . time(). time() . PHP_EOL );
+echo esc_html( 'foo: ' . time(). time() . PHP_EOL );
 
