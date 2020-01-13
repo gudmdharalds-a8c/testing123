2f8996f22fb60620242c01c73dc5906aad015d0e
Gudmundur Haraldsson <gdh1@hi.is>
2020-01-13 20:07:00 +0000

    test

--- bla-5.php
+++ bla-5.php
@@ -19,4 +19,5 @@ echo esc_html( 'nei' );
 
 echo time() . 'foo1' .  time() . 'foo2' . PHP_EOL;
 echo time() . 'foo1' .  time() . 'foo2' . PHP_EOL;
+echo time() . 'foo1' .  time() . 'foo2' . PHP_EOL;
 
