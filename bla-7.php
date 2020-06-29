1c9f6e6d37cbc0e8ba05e9388d72307783a2e625
Gudmundur Haraldsson <gdh1@hi.is>
2020-06-29 18:33:13 +0000

    test

--- bla-7.php
+++ bla-7.php
@@ -1,11 +1,3 @@
 <?php
 
-global $wpdb;
-
-        $listofthings = $wpdb->get_col( <<<EOD
-                SELECT something
-                FROM somewhere
-                WHERE someotherthing = 1
-EOD
-        ); // DB call okay ( No Warning, but Error for not caching! ).
-
+mysql_affected_rows();
