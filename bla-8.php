8a9212de59d3283ef25f084fed45199276770755
Gudmundur Haraldsson <gdh1@hi.is>
2021-04-21 15:46:40 +0000

    test

--- bla-8.php
+++ bla-8.php
@@ -1,3 +1,12 @@
 <?php
 
 echo "foo" . time() . PHP_EOL;
+
+echo "foo2" . time() . PHP_EOL;
+
+// echo "foo3" . time() . PHP_EOL;
+
+echo "foo4" . time() . PHP_EOL;
+
+echo "foo5" . time() . PHP_EOL;
+
